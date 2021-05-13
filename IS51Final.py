eb42c7cb5873c8cea1f72500591e0555ef5367fc
mtello5 <mtello5@mail.fresnostate.edu>
2021-05-13 16:32:22 -0700

    added pseudo-code

--- IS51Final.py
+++ IS51Final.py
@@ -5,3 +5,10 @@ Number of grades = length of list of intergers
 Average grade = sum of grades / # of total grades
 Percentage of grades above average = grades > average / # of total grades 
 """
+"""
+main() will open "Final.txt" and create it to a list of intergers
+
+calculate_above_average() will get the number of grades, find average grade, and find percentage of grades above average
+
+
+"""
