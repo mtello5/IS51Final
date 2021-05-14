d8554b23a979a8e1109bb6de3780a7f106cd2c03
mtello5 <mtello5@mail.fresnostate.edu>
2021-05-13 17:02:13 -0700

    finished code

--- IS51Final.py
+++ IS51Final.py
@@ -12,3 +12,25 @@ calculate_above_average() will get the number of grades, find average grade, and
 
 
 """
+def main():
+    infile = open ("Final.txt", 'r')
+    grades = [line.rstrip () for line in infile]
+    infile.close()
+    return grades
+grades = main()
+def calculate_percent_above_average():
+    infile = open ("Final.txt", 'r')
+    for i in range(len(grades)):
+        grades[i] = int(grades[i])
+    average = sum(grades) / len(grades)
+    num = 0
+    for grade in grades:
+        if grade > average:
+            num += 1
+    print("Number of grades:", len(grades))
+    print("Average grade:", average)
+    print("Percentage of grades above average: {0:.2f}%"
+                    . format(100 * num / len(grades)))
+
+main()
+calculate_percent_above_average()
